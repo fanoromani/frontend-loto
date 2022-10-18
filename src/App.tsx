9fb31b293fbea0346d22e64a02b5af1e060e3a96
Stefano Romani <fanoromani@gmail.com>
2022-10-18 13:29:41 -0300

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="h-20 p-0 m-0 flex justify-center items-center uppercase text-2xl text-white font-bold bg-megasena">
+      Mega-Sena
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        megasena: "#6BEFA3",
+        quina: "#8666EF",
+        lotofacil: "#DD7AC6",
+        lotomania: "#FFAB64",
+        timemania: "#5AAD7D",
+        diadesorte: "#BFAF83",
+      },
+    },
+  },
+  plugins: [],
+};
